5f926dc94a59e3d8c14a2700bac793e2c33dd26e
allugosbusiness-cpu <allugosbusiness@gmail.com>
2026-05-16 18:29:27 +0200

    Refactor admin.py to update model registrations

--- api/admin.py
+++ api/admin.py
@@ -1,35 +1,43 @@
 from django.contrib import admin
-from .models import Checkpoint
-from .models import FleetTruck as Truck, FleetDriver as Driver, FleetMission as Mission, TruckLocation as Location
-# Note: If Checkpoint, Cargo, Alert, and KPI are NOT in your new models.py, 
-# you must remove them from this list or the app will crash again.
-
+from .models import (
+    FleetTruck as Truck, 
+    FleetDriver as Driver, 
+    FleetMission as Mission, 
+    TruckLocation as Location, 
+    FleetMissionStop as Checkpoint,  # Use the V2 Stop as the Checkpoint
+    FleetActivity,
+    FleetDriverPerformanceDaily
+)
 
 @admin.register(Truck)
 class TruckAdmin(admin.ModelAdmin):
-    list_display = ('id', 'plate', 'driver', 'status', 'location', 'speed', 'progress')
-    search_fields = ('id', 'plate', 'driver', 'location')
-    list_filter = ('status', 'location')
-
-@admin.register(Checkpoint)
-class CheckpointAdmin(admin.ModelAdmin):
-    list_display = ('id', 'truck', 'name', 'status', 'timestamp')
-    search_fields = ('name', 'detail')
+    list_display = ('truck_identifier', 'plate', 'status') 
+    search_fields = ('truck_identifier', 'plate')
     list_filter = ('status',)
 
-@admin.register(Cargo)
-class CargoAdmin(admin.ModelAdmin):
-    list_display = ('id', 'truck', 'cargo_type', 'weight', 'origin', 'destination')
-    search_fields = ('truck__id', 'cargo_type', 'origin', 'destination')
-    list_filter = ('cargo_type',)
+@admin.register(Mission)
+class MissionAdmin(admin.ModelAdmin):
+    list_display = ('mission_number', 'status', 'priority')
+    search_fields = ('mission_number',)
+    list_filter = ('status', 'priority')
 
-@admin.register(Alert)
-class AlertAdmin(admin.ModelAdmin):
-    list_display = ('id', 'truck', 'alert_type', 'message', 'is_resolved', 'timestamp')
-    search_fields = ('truck__id', 'message')
-    list_filter = ('alert_type', 'is_resolved')
+@admin.register(Checkpoint) # This is actually FleetMissionStop
+class CheckpointAdmin(admin.ModelAdmin):
+    list_display = ('id', 'mission', 'stop_order', 'status')
+    search_fields = ('address',)
+    list_filter = ('status',)
 
-@admin.register(KPI)
-class KPIAdmin(admin.ModelAdmin):
-    list_display = ('id', 'date', 'active_trucks', 'on_time_rate', 'avg_speed', 'total_deliveries', 'critical_alerts')
-    ordering = ('-timestamp',)
+@admin.register(Driver)
+class DriverAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'status', 'on_duty')
+    search_fields = ('first_name', 'last_name', 'phone_number')
+    list_filter = ('status', 'on_duty')
+
+@admin.register(FleetActivity)
+class ActivityAdmin(admin.ModelAdmin):
+    list_display = ('truck', 'activity_type', 'timestamp')
+    list_filter = ('activity_type', 'is_critical')
+
+@admin.register(FleetDriverPerformanceDaily)
+class PerformanceAdmin(admin.ModelAdmin):
+    list_display = ('driver', 'date', 'overall_score')
