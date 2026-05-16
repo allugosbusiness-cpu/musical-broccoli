3d07ebabc763880be19677d99016483fcc7d22b8
allugosbusiness-cpu <allugosbusiness@gmail.com>
2026-05-16 18:44:52 +0200

    Refactor serializers to use updated models

--- api/serializers.py
+++ api/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models_v2 import (
+from .models import (
     FleetDriver, FleetTruck, FleetMission, FleetMissionStop, FleetMissionEvent, 
     FleetMissionDispute, FleetDriverPerformanceDaily, FleetAdminAuditLog
 )
