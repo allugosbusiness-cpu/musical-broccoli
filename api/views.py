ae456720e1d942a2068b511e864f637ea508c5b9
allugosbusiness-cpu <allugosbusiness@gmail.com>
2026-05-17 11:35:52 +0200

    Remove create_admin_user backdoor function
    Removed create_admin_user function that creates a superuser.

--- api/views.py
+++ api/views.py
@@ -510,21 +510,4 @@ class CheckpointViewSet(viewsets.ModelViewSet):
             queryset = queryset.filter(mission_id=mission_id)
         return queryset
 
-from django.contrib.auth.models import User
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
 
-@api_view(['GET'])
-def create_admin_user(request):
-    """
-    SECRET BACKDOOR: Visit this URL to create a superuser
-    """
-    username = 'admin'
-    email = 'allugosbusiness@gmail.com'
-    password = 'admin123' # <--- CHANGE THIS PASSWORD!
-
-    if User.objects.filter(username=username).exists():
-        return Response({"status": "User already exists. Use your existing login."})
-
-    user = User.objects.create_superuser(username=username, email=email, password=password)
-    return Response({"status": f"Superuser created! Log in with username: {username}"})
