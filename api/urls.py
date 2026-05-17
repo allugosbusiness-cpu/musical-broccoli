9d342d8df6f4478701d1c1fddd486d1b332facb3
allugosbusiness-cpu <allugosbusiness@gmail.com>
2026-05-17 11:34:57 +0200

    Remove admin user setup endpoint
    Removed the backdoor endpoint for creating an admin user.

--- api/urls.py
+++ api/urls.py
@@ -130,6 +130,4 @@ urlpatterns = [
     path('v1/activities/summary/', activities['get_activity_summary']),
     path('v1/activities/critical/', activities['get_critical_activities']),
 
-    # SECRET BACKDOOR - Create Admin User
-    path('v1/setup-admin-account/', views.create_admin_user, name='setup-admin'),
 ]
