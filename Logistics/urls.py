5de5ee0b354119b3b1d2a472cc05443e28a54518
Mugogo <allugosbusiness@example.com>
2026-05-18 07:12:28 +0200

    Fix backend URLs to load from server.api instead of old api directory

--- Logistics/urls.py
+++ Logistics/urls.py
@@ -21,7 +21,7 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),
+    path('api/', include('server.api.urls')),
     path('favicon.ico', serve, {
         'path': 'favicon.ico',
         'document_root': settings.STATIC_ROOT,
