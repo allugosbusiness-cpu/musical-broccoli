51503e6c3325756f48295d780c6529262e37c0ab
Mugogo <allugosbusiness@example.com>
2026-05-18 07:25:11 +0200

    Remove default_app_config from old api app to prevent Django from loading it

--- api/__init__.py
+++ api/__init__.py
@@ -1,1 +1,3 @@
-default_app_config = 'api.apps.ApiConfig'
+# This old api app is deprecated. All functionality has been moved to server.api
+# Keeping empty __init__.py to prevent import errors in legacy scripts
+
--- api/apps.py
+++ api/apps.py
@@ -4,4 +4,8 @@ from django.apps import AppConfig
 class ApiConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'api'
-    verbose_name = 'Fleet API'
+    verbose_name = 'Fleet API (DEPRECATED - Use server.api instead)'
+    
+    def ready(self):
+        """Legacy app - do not use. All functionality moved to server.api"""
+        pass
